f19b4dbb16e73368cfd4b5dcd71d13117efe0e86
Yuho Jeong <yuho8437@unist.ac.kr>
2022-04-17 15:14:52 +0900

    refactor: Delete all files which are not related to NER task

--- models/model_config.py
+++ models/model_config.py
@@ -8,21 +8,6 @@
 from transformers import BertConfig
 
 
-class BertForQAConfig(BertConfig):
-    def __init__(self, **kwargs):
-        super(BertForQAConfig, self).__init__(**kwargs)
-        self.hidden_size = kwargs.get("hidden_size", 768)
-        self.multi_layer_classifier = kwargs.get("multi_layer_classifier", True)
-        self.truncated_normal = kwargs.get("truncated_normal", True)
-
-class BertForSequenceClassificationConfig(BertConfig):
-    def __init__(self, **kwargs):
-        super(BertForSequenceClassificationConfig, self).__init__(**kwargs)
-        self.hidden_dropout_prob = kwargs.get("hidden_dropout_prob", 0.0)
-        self.num_labels = kwargs.get("num_labels", 2)
-        self.hidden_size = kwargs.get("hidden_size", 768)
-        self.truncated_normal = kwargs.get("truncated_normal", False)
-
 class BertForQueryNERConfig(BertConfig):
     def __init__(self, **kwargs):
         super(BertForQueryNERConfig, self).__init__(**kwargs)
